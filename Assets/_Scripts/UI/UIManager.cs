1f70219a8a78d7ee0d9b2fbaf24be062ff13e642
timmi <timmid999@gmail.com>
2025-03-16 14:15:33 -0700

    Created UIManager script to change the lives text and to set the game over panel to true when lives hits 0.

---
+++ Assets/_Scripts/UI/UIManager.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI livesText;
+    [SerializeField] private GameObject gameOverPanel;
+
+    public void UpdateLives(int lives)
+    {
+        livesText.text = "Lives: " + lives;
+    }
+
+    public void ShowGameOverScreen()
+    {
+        gameOverPanel.SetActive(true);
+    }
+}
